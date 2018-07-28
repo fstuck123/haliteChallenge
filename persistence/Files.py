05196e3f3c507d6c66d7e384efb22d4faa30d4db
Fabian Stuckmann <fabian.stuckmann@outlook.de>
2018-07-28 14:55:15 +0200

    able to pickle files and unpickle

---
+++ persistence/Files.py
@@ -0,0 +1,25 @@
+import pickle
+import os
+from states.HaliteStates import HaliteStates
+
+
+class Files:
+    directory =""
+    file =""
+
+
+    def __init__(self, relativeDirectory):
+        self.directory = os.path.dirname(os.path.realpath(__file__)) + relativeDirectory
+
+
+    def accessFile(self, file):
+        bytearray = open(self.directory + file, "rb");
+        # probably have to import HaliteStates so that pickle knows which class the bytes belong to
+        object = pickle.load(bytearray)
+        return object
+
+
+    def saveState(self, file, haliteState):
+        fileSystem  = open(self.directory + file, "wb");
+        pickle.dump(haliteState, fileSystem);
+
