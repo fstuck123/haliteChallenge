440619a1b3ea1e7d1e927a564dac5e56b7e37bb7
Fabian Stuckmann <fabian.stuckmann@outlook.de>
2018-07-31 16:12:11 +0200

    Added multiple interfaces

--- core/algorithms/SARSA.py
+++ core/algorithms/SARSA.py
@@ -1,2 +1,15 @@
 
 
+
+
+
+class SARSA:
+    def __init__(self, policy, actionValueFunction, stateValueFunction, learningRate, epsilon):
+        self.policy = policy
+        self.actionValueFunction = actionValueFunction
+        self.stateValueFunction = stateValueFunction
+        self.learningRate = learningRate
+        self.epsilon = epsilon
+
+
+    def execute(self):
--- core/wrapper/Policy.py
+++ core/wrapper/Policy.py
@@ -1,4 +1,17 @@
 import abc
 
 
-class 
+class Policy(object, metaclass=abc.ABCMeta):
+
+    @abc.abstractclassmethod
+    def init(cls, greedyPolicy, epsilonPolicy):
+        raise NotImplementedError("init must be implemented")
+
+    @abc.abstractclassmethod
+    def getEpsilonGreedyAction(cls, state, actionValueFunction):
+        raise NotImplementedError("epsilon greedy must be added")
+
+
+    @abc.abstractclassmethod
+    def getGreedyAction(cls, state, actionValueFunciton):
+        raise NotImplementedError("greedy algorithm must be added")
