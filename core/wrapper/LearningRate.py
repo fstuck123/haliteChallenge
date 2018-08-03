6fd60fabee19b025bec6527c2fd4ec9df8a41317
Fabian Stuckmann <fabian.stuckmann@outlook.de>
2018-08-03 11:28:52 +0200

    updated learningrate

--- core/wrapper/LearningRate.py
+++ core/wrapper/LearningRate.py
@@ -5,3 +5,7 @@ class LearningRate(object, metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def decrease(self):
         raise NotImplementedError("Decrease Learningrate is not implemented yet")
+
+    @abc.abstractmethod
+    def getValue(self):
+        raise NotImplementedError("get value LearningRate not implemented")
