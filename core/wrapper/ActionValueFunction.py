385fa4c0119059f52b5ca8b640a24abf035fe084
Fabian Stuckmann <fabian.stuckmann@outlook.de>
2018-08-03 10:37:45 +0200

    updated interface of actionvaluefunction

--- core/wrapper/ActionValueFunction.py
+++ core/wrapper/ActionValueFunction.py
@@ -10,3 +10,8 @@ class ActionValueFunction(object, metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def update(self, state, action, nextState, stateValueFunction, learningRate):
         raise NotImplementedError("update ActionValueFunction not implemented yet")
+
+
+    @abc.abstractmethod
+    def evaluate(self, state):
+        raise NotImplementedError("evaluation of ActionValueFunction has not been implemented yet")
