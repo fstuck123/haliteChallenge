3d4d007a011e00eabeac011a99b691e2ae5e0a89
Fabian Stuckmann <fabian.stuckmann@outlook.de>
2018-08-03 10:38:40 +0200

    init Action-value function complete with simple CNN

--- core/implementations/ActionValueFunctionImpl.py
+++ core/implementations/ActionValueFunctionImpl.py
@@ -13,13 +13,32 @@ class ActionValueFunctionImpl1(ActionValueFunction):
         self.x = tf.placeholder(tf.float32, [None, self.inputDimensions])
         self.y = tf.placeholder(tf.float32, [None, self.outputDimensions])
 
+        widthFilter = 5;
+        padding = 0
+        stride = 2
+        poolSize =3
+
+        # calculate final dimensions before the fully connected network begins
+        #conv layer
+        finalX = (self.inputDimensions.x() - widthFilter + 2 * padding) / (stride + 1)
+        #max pool layer
+        finalX = (finalX - poolSize) / (poolSize + 1)
+        finalX = (finalX - widthFilter + 2 * padding) / (stride + 1)
+        # max pool layer
+        finalX = (finalX - poolSize) / (poolSize + 1)
+        finalY = (self.inputDimensions.y() - widthFilter + 2 * padding) / (stride + 1)
+        # max pool layer
+        finalY = (finalY - poolSize) / (poolSize + 1)
+        finalY = (finalY - widthFilter + 2 * padding) / (stride + 1)
+        # max pool layer
+        finalY = (finalY - poolSize) / (poolSize + 1)
 
         self.weights = {
             # 5x5 conv
-            'wc1': tf.Variable(tf.random_normal([5,5,self.inputDimensions.dimensions,32])),
-            'wc2': tf.Varialbe(tf.random_normal([5,5, 32, 64])),
+            'wc1': tf.Variable(tf.random_normal([widthFilter,widthFilter,self.inputDimensions.dimensions,32])),
+            'wc2': tf.Varialbe(tf.random_normal([widthFilter,widthFilter, 32, 64])),
             # fully connected
-            'wd1': tf.Variable(tf.random_normal([, 1024])),
+            'wd1': tf.Variable(tf.random_normal([finalX * finalY * 64, 1024])),
             'out': tf.Variable(tf.random_normal([1024, self.outputDimensions]))
 
         }
@@ -33,15 +52,35 @@ class ActionValueFunctionImpl1(ActionValueFunction):
         }
 
         # define net
-        conv1 = self.conv(self.x, self.weights['wc1'], self.bias['bc1']);
-        conv2 = self.conv(conv1, self.weights['wc2'], self.bias['bc2']);
+        conv1 = self.conv(self.x, self.weights['wc1'], self.bias['bc1'], stride);
+        conv1 = self.maxpool2d(conv1, k=poolSize)
+        conv2 = self.conv(conv1, self.weights['wc2'], self.bias['bc2'], stride);
+        conv2 = self.maxpool2d(conv2, k=poolSize)
+
+        #reshape conv2 to be a two dimensional input
+        fc1 = tf.reshape(conv2, [-1, self.weights['wd1'].get_shape().as_list()[0]])
+
+        fc1 = tf.add(tf.matmul(fc1, self.weights['wd1']), self.bias['bd1'])
+        fc1 = tf.nn.relu(fc1)
+
+        dropout = 0.5
+        fc1 = tf.nn.dropout(fc1, dropout)
+
+        self.out = tf.add(tf.matmul(fc1, self.weights['out']), self.bias['out'])
 
 
 
 
 
 
-    def conv(self, x, W, b, strides = 1):
+
+
+
+    def conv(self, x, W, b, strides = 2):
         x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME');
         x = tf.nn.bias_add(x, b)
         return tf.nn.relu(x)
+
+    def maxpool2d(self, x, k=2):
+        return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
+                              padding='SAME')
